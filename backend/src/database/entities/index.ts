bc9284ea602badad64b3fc4389acda6575ffe23b
star <snaoyam@gmail.com>
2022-09-03 18:54:07 +0900

    Add entities index.ts

---
+++ backend/src/database/entities/index.ts
@@ -0,0 +1,17 @@
+import { Activity } from './activity.entity';
+import { Group } from './group.entity';
+import { GroupMissionDate } from './group_mission_date.entity';
+import { GroupMissionDateList } from './group_mission_date_list.entity';
+import { MissionCategory } from './mission_category.entity';
+import { User } from './user.entity';
+import { UserGroup } from './user_group.entity';
+
+export {
+  Activity,
+  Group,
+  GroupMissionDate,
+  GroupMissionDateList,
+  MissionCategory,
+  User,
+  UserGroup,
+};

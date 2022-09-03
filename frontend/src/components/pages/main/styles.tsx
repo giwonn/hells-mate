17c3b8cf4712229d633b36955fd8f6a0b2a23fc3
장재원 <jaychang99@scg.skku.ac.kr>
2022-09-03 23:57:31 +0900

    feat(frontend): index.tsx 페이지 구현

--- frontend/src/components/pages/main/styles.tsx
+++ frontend/src/components/pages/main/styles.tsx
@@ -5,17 +5,17 @@ export const StyledMainPageContainer = styled.div`
   padding: 50px 16px 16px;
 `;
 
-export const MainPageTopRowContainer  = styled.div`
+export const MainPageTopRowContainer = styled.div`
   display: flex;
   justify-content: flex-end;
-`
+`;
 
 export const MainPageCalendarContaier = styled.div`
   margin-bottom: 48px;
-`
+`;
 
 export const MainPageChallengesContainer = styled.div`
   display: flex;
   flex-direction: column;
   row-gap: 24px;
-`
+`;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,15 +1,40 @@
+import { useState } from "react";
 import type { NextPage } from "next";
+import Image from "next/image";
+import Calendar from "components/pages/main/Calendar";
+import Challenge from "components/pages/main/Challenge";
+import {
+  MainPageCalendarContaier,
+  MainPageChallengesContainer,
+  MainPageTopRowContainer,
+  StyledMainPageContainer,
+} from "components/pages/main/styles";
+import { MOCKUP_CHALLENGES } from "mockups/challenges";
 
-declare global {
-  interface Window {
-    Kakao: any;
-  }
-}
+import rankingIcon from "/public/icons/ranking_icon.svg";
+
+// declare global {
+//   interface Window {
+//     Kakao: any;
+//   }
+// }
 const Home: NextPage = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  console.log("SET", selectedDate);
   return (
-    <>
-      <div>Hello World</div>
-    </>
+    <StyledMainPageContainer>
+      <MainPageTopRowContainer>
+        <Image alt="ranking icon" src={rankingIcon.src} width={20} height={27} />
+      </MainPageTopRowContainer>
+      <MainPageCalendarContaier>
+        <Calendar onDateChange={setSelectedDate} selectedDate={selectedDate} />
+      </MainPageCalendarContaier>
+      <MainPageChallengesContainer>
+        {MOCKUP_CHALLENGES.map((challenge, index) => (
+          <Challenge key={index} {...challenge} />
+        ))}
+      </MainPageChallengesContainer>
+    </StyledMainPageContainer>
   );
 };
 

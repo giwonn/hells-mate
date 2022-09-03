aa10e3d8701a588b8d1e6589c11ffa7445204668
Allen <allendev@alphacode.co.kr>
2022-09-03 22:31:38 +0900

    카카오 SDK 추가

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from "next";
 
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
 const Home: NextPage = () => {
   return (
     <>
+      <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
       <div>Hello World</div>
     </>
   );

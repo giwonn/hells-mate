7a0cb4f407fceba0fd8a1f794a7b575c7eadad9c
장재원 <jaychang99@scg.skku.ac.kr>
2022-09-03 23:41:15 +0900

    refactor(frontend): LoginButton.tsx 이동

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,12 +1,16 @@
 import { NextPage } from "next";
-import kakaoLogin from "./api/kakaoLogin";
 import Image from "next/image";
+import Script from "next/script";
+
+import kakaoLogin from "../../pages/api/kakaoLogin";
 
 const LoginButton: NextPage = () => {
   return (
     <>
+      <Script src="https://developers.kakao.com/sdk/js/kakao.js" />
       <a onClick={kakaoLogin}>누르면 로그인 요청함</a>
       <Image
+        alt="kakao login button"
         src="/kakao_login_medium_narrow.png"
         layout="intrinsic"
         width={150}

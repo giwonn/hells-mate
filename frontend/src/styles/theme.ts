88a966eafd32d111c4a2ec75cc2519551b666e4d
장재원 <jaychang99@scg.skku.ac.kr>
2022-09-03 09:25:33 +0900

    chore(frontend): theme.ts 에 color 추가

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -1,6 +1,18 @@
 import { Theme } from "@emotion/react";
 
 export const theme: Theme = {
+  colors: {
+    primary100: "#F8F9FD",
+    primary200: "#ECF0F9",
+    primary600: "#7CA4F9",
+    primary700: "#5F7BFF",
+    orange: "#FB7548",
+    mint: "#38BDAC",
+    background: "#EFF3F6",
+    white: "#FFFFFF",
+    black500: "#677A90",
+    black900: "#191F28",
+  },
   paddings: {
     globalPadding: "16px",
   },
--- frontend/src/types/emotion.d.ts
+++ frontend/src/types/emotion.d.ts
@@ -2,6 +2,18 @@ import "@emotion/react";
 
 declare module "@emotion/react" {
   export interface Theme {
+    colors: {
+      primary100: string;
+      primary200: string;
+      primary600: string;
+      primary700: string;
+      orange: string;
+      mint: string;
+      background: string;
+      white: string;
+      black500: string;
+      black900: string;
+    };
     paddings: {
       globalPadding: string;
     };
